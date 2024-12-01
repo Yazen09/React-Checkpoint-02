33db592938e132b4fee5a9afbbe11e9a568c1b70
Yazen09 <yazenabouchendi1998@gmail.com>
2024-12-01 12:35:33 +0100

    git add .

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PlayerList from './Components/Playerlist';
+
+
+function App() {
+  return (
+    
+   <div className="App">
+
+    <PlayerList/>
+      
+    </div>
+  );
+}
+
+export default App;
